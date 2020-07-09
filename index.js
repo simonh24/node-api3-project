100c060148d17f355791d2ceef2a3a5ecf8dc37b
Simon <simonh72498@gmail.com>
2020-07-08 22:21:55 -0400

    mvp

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code away!
+const server = require("./server.js");
+
+const PORT = 8000;
+server.listen(PORT, () => {
+    console.log(`running on port ${PORT}`);
+});
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
+// const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
+
 const server = express();
+
+server.use(express.json());
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +14,11 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${new Date().toISOString()} || ${req.method} to ${req.url}`);
+  next();
+}
+
+server.use("/api/users", userRouter);
 
 module.exports = server;
