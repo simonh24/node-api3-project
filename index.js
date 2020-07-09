f18a45f42173d260b382819d608d4288ab893e08
Simon <simonh72498@gmail.com>
2020-07-09 15:26:35 -0400

    port change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
 const server = require("./server.js");
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
     console.log(`running on port ${PORT}`);
 });
